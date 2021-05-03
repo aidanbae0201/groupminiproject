04af4d62dd3b27e8f69abd7c94ecc1bf41ccb9a9
JYB <22000328@handong.edu>
2021-05-03 21:55:15 +0900

    Create reserve.h

---
+++ reserve.h
@@ -0,0 +1,13 @@
+typedef struct Reserve{
+    int nop; // 인원수
+    char stid[10]; // 학번
+    int date; // 날짜
+    int month; // 달
+    int inith; // 시작시간
+    int endh; // 마치는 시간
+}Reserve;
+void printCalendar(Reserve *r[], int count, int month);
+void searchNop(Reserve *r[],int count);
+void searchStid(Reserve *r[],int count);
+void saveFile(Reserve *r[], int count);
+void addReserve(Reserve *r[], int count);
