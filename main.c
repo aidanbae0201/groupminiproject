b279be56acb3bb11bef1212786000186d2770137
JYB <22000328@handong.edu>
2021-05-06 22:28:22 +0900

    Created main.c and added the skeleton for later use

--- main.c
+++ main.c
@@ -4,8 +4,30 @@
 
 int main()
 {
-
-
+    int count=0;
+    int index=0;
+    Reserve *r[100];
+    int menu = loadMenu();
+    while(1)
+    {
+        if(menu==0)
+            break;
+        if(menu==3 || menu==4 || menu==5 || menu==6)
+        {
+            if(count==0) continue;
+        }
+        if(menu==2)
+        {
+            r[index] = (Reserve*)malloc(sizeof(Reserve));
+            count += addReserve(r[index++]);
+        }
+        if(menu==1)
+        {
+            printCalendar(r,count);
+        }
+    }
+    
+    
 
 
 
